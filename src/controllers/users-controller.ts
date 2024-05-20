2bbc616b11933be166889bd2e78e124d7a3fac16
Leonardo Santos <leogilberto2010@hotmail.com>
2024-05-20 09:59:57 -0300

    Criado os arquivos de Controller e Route de Users

---
+++ src/controllers/users-controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+
+import knex from '../config/knex';
+
+class UsersController {
+  async index(req: Request, res: Response) {
+    try {
+      const response = await knex('users').select('*');
+
+      return res.json(response);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({
+          error: err.message,
+        });
+      }
+    }
+  }
+}
+
+export default new UsersController();
