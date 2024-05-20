e0b40140279badb08a468b2712075a34e6d68250
Leonardo Santos <leogilberto2010@hotmail.com>
2024-05-20 10:06:47 -0300

    Configurado rota que chama a função show do usersController

--- src/routes/users-route.ts
+++ src/routes/users-route.ts
@@ -5,5 +5,6 @@ import usersController from '../controllers/users-controller';
 const router = Router();
 
 router.get('/', usersController.index);
+router.get('/:id', usersController.show);
 
 export default router;
