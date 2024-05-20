c26db090b248774ba6989a8e37a1be66785e8ee6
Leonardo Santos <leogilberto2010@hotmail.com>
2024-05-20 16:46:14 -0300

    Adicionado a função loginRequired nas rotas update e delete para que só funcione caso o usuário esteja logado

--- src/routes/users-route.ts
+++ src/routes/users-route.ts
@@ -2,12 +2,14 @@ import { Router } from 'express';
 
 import usersController from '../controllers/users-controller';
 
+import { loginRequired } from '../midleware/login-required';
+
 const router = Router();
 
 router.get('/', usersController.index);
 router.get('/:id', usersController.show);
 router.post('/', usersController.store);
-router.put('/:id', usersController.update);
-router.delete('/:id', usersController.delete);
+router.put('/:id', loginRequired, usersController.update);
+router.delete('/:id', loginRequired, usersController.delete);
 
 export default router;
