57459789640ad20d5d79653e2c191b24afbd550a
Leonardo Santos <92178094+LeoSantosp2@users.noreply.github.com>
2026-07-12 15:19:23 -0300

    refactor(db): drop token_auth column now that JWT is stateless

--- src/repositories/login-repository.ts
+++
@@ -1,9 +0,0 @@
-import prisma from '../config/prisma';
-
-import { LoginRequest } from '../interfaces/login-request';
-
-export const login = async (login: LoginRequest, token: string) =>
-  prisma.users.update({
-    data: { token_auth: token },
-    where: { email: login.email },
-  });
