1daa5acacdd96e1146bbc17cb78cd64904259b46
Leonardo Santos <92178094+LeoSantosp2@users.noreply.github.com>
2026-07-21 20:11:32 -0300

    build(prisma): add standalone config and switch to mariadb adapter

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,5 +1,15 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import env from './env';
 
-const prisma = new PrismaClient();
+const adapter = new PrismaMariaDb({
+  host: env.DATABASE_HOST,
+  port: Number(env.DATABASE_PORT),
+  user: env.DATABASE_USERNAME,
+  password: env.DATABASE_PASSWORD,
+  database: env.DATABASE,
+});
+
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
