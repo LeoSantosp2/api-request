d8ea9db2f47142fcaefa0f3d15dd40d0658dfccf
Leonardo Santos <92178094+LeoSantosp2@users.noreply.github.com>
2026-09-18 10:51:07 -0300

    chore(lint): silence unused next param in error handler

--- src/presentation/middleware/error.handler.ts
+++ src/presentation/middleware/error.handler.ts
@@ -23,6 +23,7 @@ export function errorHandler(
   err: Error,
   req: Request,
   res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction,
 ): Response {
   if (err instanceof HttpError) {
